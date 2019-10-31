ccd7341d5951943152ef2eca00e81e5a0ed3d5a5
leandro <lekrygoski@gmail.com>
2019-10-31 19:45:49 -0300

    cadastro usuarios
    classe para criar uma nova conta para o usuario

---
+++ celig/Celig.Model/CadastroUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celig.Model
+{
+    public class CadastroUser
+    {
+        public CadastroUser(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public CadastroUser()
+        {
+
+        }
+        public string userName { get; set; }
+        public string password { get; set; }
+
+
+    }
+}
---
+++ celig/Celig.Model/CadastroUserDao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Celig.Model
+{
+    public class CadastroUserDao
+    {
+        private DataBase db;
+
+        public CadastroUserDao()
+        {
+            //instancia a classe DataBase
+            db = new DataBase();
+        }
+
+        public int CadastroUsuario(CadastroUser dados)
+        {
+            int resultado;
+            string query = "INSERT INTO Login (userName, password)" + "VALUES (@user, @pass)";
+
+            try
+            {
+                db.OpenConn();
+                db.addParameter("@user", dados.userName, DbType.String);
+                db.addParameter("@pass", dados.password, DbType.String);
+                resultado = db.ExecuteNoQuery(query);
+                return resultado;
+            }
+            
+            catch (Exception)
+            {
+
+                throw new Exception ("Erro no Banco de Dados");
+            }
+            finally
+            {
+                db.CloseConn();
+            }
+
+
+        }
+
+
+    }
+}
